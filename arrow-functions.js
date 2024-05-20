52ef696f05e7265998ebb3f37b506b46667e426a
ecolus <ecolus48@gmail.com>
2024-05-20 17:14:04 +0100

    idk lmao

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b
+}
 
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a + b
+}
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => a+b;
+
 // Implicit Returns
 
+const saySomething = message => console.log(message)
+saySomething('Hello there!')
+
+const sayHello = () => console.log(hello)
+sayHello()
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p> Hello!
+    This is a 
+    multiline string!
+    </p>`
+)
